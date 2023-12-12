d404407fa216990677ca57df67077cf02975e430
TilekSh1199 <tilek1199@gmail.com>
2023-12-12 11:59:26 +0600

    last commit

--- server/server.js
+++ server/server.js
@@ -10,7 +10,7 @@ dotenv.config();
 // });
 
 const openai = new OpenAI({
-  apiKey: process.env.OPENAI_API_KEY,
+  apiKey: process.env.OPEN_API_KEY,
 });
 
 // const openai = new OpenAIApi(configuration);
@@ -29,9 +29,19 @@ app.post("/", async (req, res) => {
   try {
     const prompt = req.body.prompt;
 
-    const response = await openai.createCompletion({
-      model: "text-davinci-003",
-      prompt: `${prompt}`,
+    const response = await openai.chat.completions.create({
+      model: "gpt-4-1106-preview",
+      messages: [{ role: "system", content: "You are a helpful assistant." }],
+      messages: [
+        {
+          role: "system",
+          content: "You are a helpful assistant.",
+        },
+        {
+          role: "user",
+          content: prompt,
+        },
+      ],
       temperature: 0, // Higher values means the model will take more risks.
       max_tokens: 3000, // The maximum number of tokens to generate in the completion. Most models have a context length of 2048 tokens (except for the newest models, which support 4096).
       top_p: 1, // alternative to sampling with temperature, called nucleus sampling
@@ -39,12 +49,18 @@ app.post("/", async (req, res) => {
       presence_penalty: 0, // Number between -2.0 and 2.0. Positive values penalize new tokens based on whether they appear in the text so far, increasing the model's likelihood to talk about new topics.
     });
 
+    const choice = response.choices[0];
+    const message = choice.message.content;
+
     res.status(200).send({
-      bot: response.data.choices[0].text,
+      bot: message,
     });
   } catch (error) {
-    console.error(error);
-    res.status(500).send(error || "Something went wrong");
+    console.error(
+      "Error making API request:",
+      error.response ? error.response.data : error.message
+    );
+    res.status(500).send(error || "Не работает");
   }
 });
 
